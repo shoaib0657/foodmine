f1989808c8c9274032bc3cdde419ccbdf3f96e5a
Mohammad Shoaib Ansari <mdshoaibansari0307@gmail.com>
2023-12-13 04:29:29 +0530

    Remove sample data import.

--- frontend/src/app/services/food.service.ts
+++ frontend/src/app/services/food.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Food } from '../shared/models/Food';
-import { sample_foods, sample_tags } from 'src/data';
 import { Tag } from '../shared/models/Tag';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
