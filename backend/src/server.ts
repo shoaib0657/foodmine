b8c62c372a01360b6b49ce44e419027ae79cceb1
Mohammad Shoaib Ansari <mdshoaibansari0307@gmail.com>
2023-12-18 00:06:58 +0530

    deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config();
 
+import path from 'path';
 import express from "express";
 import cors from "cors"
 import foodRouter from "./routers/food.router";
@@ -25,7 +26,12 @@ app.use("/api/users", userRouter);
 
 app.use("/api/orders", orderRouter);
 
-const port = 5000;
+app.use(express.static('public'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname,'public', 'index.html'))
+})
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server started at http://localhost:` + port);
