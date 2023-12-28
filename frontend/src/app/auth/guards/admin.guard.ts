0d0802704daeebe96dae568955a96d681b30f358
Mohammad Shoaib Ansari <mdshoaibansari0307@gmail.com>
2023-12-28 08:20:01 +0530

    added comments

--- frontend/src/app/auth/guards/admin.guard.ts
+++ frontend/src/app/auth/guards/admin.guard.ts
@@ -5,16 +5,21 @@ import { UserService } from 'src/app/services/user.service';
 
 export const adminGuard: CanActivateFn = (route, state) => {
 
+  // Injecting Angular services using the 'inject' function
   const router = inject(Router);
   const userService = inject(UserService);
   const toastrService = inject(ToastrService);
 
+  // Checking if the current user is logged in and has admin privileges
   if(userService.currentUser && userService.currentUser.isAdmin) {
+    // If admin, allow access to the route
     return true;
   }
 
+  // If not admin, show an error message and navigate to the dashboard
   toastrService.error('Access Denied');
   router.navigate(['/dashboard']);
 
+  // Return false to deny access to the route
   return false;
 };
--- frontend/src/app/shared/interceptors/loading.interceptor.ts
+++ frontend/src/app/shared/interceptors/loading.interceptor.ts
@@ -9,13 +9,15 @@ import {
 import { Observable, tap } from 'rxjs';
 import { LoadingService } from 'src/app/services/loading.service';
 
-var pendingRequests = 0;
+var pendingRequests = 0; // Counter to keep track of pending HTTP requests
 
+// Injectable class to intercept HTTP requests for showing loading indicators
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
 
   constructor(private loadingService: LoadingService) { }
 
+  // Intercept method to handle HTTP requests and responses
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
     // if (!(request.body instanceof FormData)) {
@@ -23,18 +25,21 @@ export class LoadingInterceptor implements HttpInterceptor {
     //   pendingRequests++;
     // }
 
+    // Show loading indicator for all requests
     this.loadingService.showLoading();
     pendingRequests++;
 
+    // Continue handling the HTTP request and response
     return next.handle(request).pipe(
       tap({
         next: (event) => {
+          // Handle loading indicator when a response is received
           if (event.type === HttpEventType.Response) {
             this.handleHideLoading();
           }
         },
         error: (_) => {
-          this.handleHideLoading();
+          this.handleHideLoading(); // Handle loading indicator in case of an error
         }
       })
     );
@@ -42,7 +47,7 @@ export class LoadingInterceptor implements HttpInterceptor {
 
   handleHideLoading() {
     pendingRequests--;
-    if (pendingRequests === 0) {
+    if (pendingRequests === 0) {  // Hide loading indicator when there are no pending requests
       this.loadingService.hideLoading();
     }
   }
