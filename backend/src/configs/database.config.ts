43a45c541cafc1f665a03a3b570daded70347d2e
Mohammad Shoaib Ansari <mdshoaibansari0307@gmail.com>
2024-01-11 00:12:48 +0530

    updated dbConnect

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -1,10 +1,7 @@
 import { connect, ConnectOptions } from "mongoose";
 
 export const dbConnect = () => {
-    connect(process.env.MONGODB_URL!, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    } as ConnectOptions).then(
+    connect(process.env.MONGODB_URL!).then(
         () => {
             console.log("Database connected successfully");
         },
