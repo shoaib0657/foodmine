113ac118cd325e0ad9e9f5bf4f30f6346179c3db
Mohammad Shoaib Ansari <mdshoaibansari0307@gmail.com>
2024-09-06 03:57:18 +0530

    vercel deploy

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,7 +1,7 @@
 // const BASE_URL = 'http://localhost:5000'
 import { environment } from "src/environments/environment";
 
-const BASE_URL = environment.production ? 'https://foodmine-backend-five.vercel.app/' : 'http://localhost:5000';
+const BASE_URL = environment.production ? 'https://foodmine-backend-five.vercel.app' : 'http://localhost:5000';
 
 export const FOODS_URL = BASE_URL + '/api/foods';
 export const FOODS_TAGS_URL = FOODS_URL + '/tags';
